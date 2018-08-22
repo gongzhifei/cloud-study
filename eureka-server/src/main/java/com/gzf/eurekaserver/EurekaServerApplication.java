49c5c6a1a5a32608219f64d434c1665d864cac6b
gongzhifei <gongzhifeil@163.com>
2018-08-22 11:21:33 +0800

    add coede

--- eureka-server/src/main/java/com/gzf/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/gzf/eurekaserver/EurekaServerApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * @author Phil
+ */
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
