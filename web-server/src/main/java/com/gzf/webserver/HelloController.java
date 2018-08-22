b22e28cea934aaf6c10cdb590171ae537e9f8c1d
gongzhifei <gongzhifeil@163.com>
2018-08-22 17:26:44 +0800

    commit web-server

---
+++ web-server/src/main/java/com/gzf/webserver/HelloController.java
@@ -0,0 +1,14 @@
+package com.gzf.webserver;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @Value("${name}")
+    private String name;
+
+
+
+}
